96d51834227569d93e54884db7d42351c3c075ca
Alexandre Zin <alexandre.zin.silva@gmail.com>
2020-05-15 12:07:16 -0300

    Dao Interfaces and Factory

---
+++ jdbc_project/src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDao{
+
+	@Override
+	public void insert(Department department) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Department department) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteId(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Department getId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ jdbc_project/src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(Seller Seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller Seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteId(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller getId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
